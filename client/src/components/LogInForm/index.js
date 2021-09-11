e86ee731a14334d2f239076c4c2d146653a20fff
Jared Donovan <jdono100@gmail.com>
2021-09-10 21:02:08 -0500

    fixing login

--- client/src/components/LogInForm/index.js
+++ client/src/components/LogInForm/index.js
@@ -2,15 +2,12 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 
 import Auth from '../../utils/auth';
-
 import { LOGIN_USER } from '../../utils/mutations';
-
 
 export default function LogInForm() {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
 
-  const [loginUser, { error }] = useMutation(LOGIN_USER);
+  const [loginUser] = useMutation(LOGIN_USER);
 
   
   const handleInputChange = (event) => {
@@ -38,15 +35,15 @@ export default function LogInForm() {
       password: ''
     });
   };
-
   
   return (
     <div>
       <form className='p-3' onSubmit={handleFormSubmit}>
-        <input type='email' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='john@website.com' />
-        <input type='password' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='password' />
+        <input onChange={handleInputChange} type='email' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='john@website.com' />
+        <input onChange={handleInputChange} type='password' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='password' />
         <button 
-          disabled={!(userFormData.email && userFormData.password)}type='submit' className='w-full px-4 py-3 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-x-75 transition-transform'>
+          type='submit' className='w-full px-4 py-3 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-x-75 transition-transform'
+        >
           Submit
         </button>
       </form>
--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+
+import Auth from '../../utils/auth';
+import { CREATE_USER } from '../../utils/mutations';
 
 export default function SignUpForm() {
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: ''});
+  const [createUser] = useMutation(CREATE_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await createUser({
+        variables: {...userFormData}
+      });
+
+      if (!data) {
+        throw new Error('something went wrong!');
+      }
+      console.log(data);
+      Auth.login(data.createUser.token);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: ''
+    });
+  };
+
   return (
     <div>
-      
+      <form className='p-3' onSubmit={handleFormSubmit}>
+      <input onChange={handleInputChange} type='text' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='username' />
+        <input onChange={handleInputChange} type='email' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='john@website.com' />
+        <input onChange={handleInputChange} type='password' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='password' />
+        {/* <input onChange={handleInputChange} type='password' className='w-full mb-3 placeholder-gray-400 placeholder-opacity-70 px-4 py-3 rounded-full' placeholder='confirm password' /> */}
+        <button 
+          type='submit' className='w-full px-4 py-3 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-x-75 transition-transform'>
+          Submit
+        </button>
+      </form>
     </div>
   )
 };
