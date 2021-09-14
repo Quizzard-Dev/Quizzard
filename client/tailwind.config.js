208e78c179d8df5df226550f6c92890e48430758
Jared Donovan <jdono100@gmail.com>
2021-09-14 09:49:19 -0500

    styling for creator almost done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,13 +11,28 @@ module.exports = {
           main: "#497FEB",
           lighter: "#91B0EE",
           darker: "#3742D4",
+          darkerer: "#262E94",
+          darkest: "#161A54",
           bluegray: "#728ABA",
+          darkerpurple: "#5731EB",
+          royalpurple: "#7222D4",
+          darkroyal: "#501894",
+          lightergold: "#A1872A",
+          gold: "#D4B337",
+          darkergold: "#876D08",
+          berry: "#7A4B78",
+          lightmagenta: "#C73EC0",
+          magenta: "#AD13A5",
+          darkmagenta: "#912D8C",
+          grass: "#76C752",
+          forest: "#5A7A4B",
           aliceblue: "#F0F8FF",
           smoke: "rgba(0, 0, 0, 0.25)"
         }
       },
       height: {
         ten: '10%',
+        sixty: '60%',
         ninety: '90%',
         eightyvh: '80vh'
       },
