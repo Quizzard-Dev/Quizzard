79160bb24efd6428ff8bf8b1ad5c115002be7530
aik <anthonykrueger0@gmail.com>
2021-09-17 15:41:12 -0500

    heroku fix

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   }
 
   app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/build'), function(err) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
        if (err) {
         res.status(500).send(err)
        }
